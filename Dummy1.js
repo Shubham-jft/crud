ed0e0997f06788904c962a39bdbdee57feb21117
Shubham-jft <shubham.kumar@jellyfishtechnologies.com>
2021-02-19 17:36:49 +0530

    commit 2

--- Dummy1.js
+++ Dummy1.js
@@ -1,41 +1,52 @@
-fetchData();
-var selectedRow = null;
-// console.log(fetchData())
+var selectedRow=null;
+var forData;
 var x=1;
-function rowcount()
+function tableRowCount()
 {
-       x=document.getElementById("Tdata").rows.length;
-        console.log(x);
+   x = document.getElementById("details").rows.length;
 }
-function onformsubmit()
+function onFormSubmit(){
+  var FormData=readFormData(); 
+  if(selectedRow==null)
+  insertNewRecord(FormData);
+  else
+  updateRecord(FormData);
+  resetForm();
+  fetchData();
+  tableRowCount();
+}
+function readFormData()
 {
-        var formdata=getdata();
-        if(selectedRow==null)
-        insertdata(formdata);
-        else
-        updateRecord(formdata);
-        resetdata();
-        rowcount();
-}
+  formData={
+    id:x,
+    name:document.getElementById("name").value,
+    username:document.getElementById("username").value,
+    phone:document.getElementById("phone").value,
+    email:document.getElementById("email").value
+  }
+  if(!localStorage.getItem("data")){
+    localStorage.setItem('data',data)
+  }
+  else
+  {
+  let stored = localStorage.getItem('data')
+  let stored_obj=JSON.parse(stored);
+  // var stored=JSON.parse(localStorage.getItem("data"))
+  // console.log(stored_obj)
+  stored_obj.push(formData)
+  console.log(stored_obj)
+  let myObj=JSON.stringify(stored_obj)
+  localStorage.setItem("data",myObj)
+  console.log(localStorage.getItem("data" ))
+  }
+  var FormData={};
+  FormData["id"]=x;
+  FormData["name"]=document.getElementById("name").value; 
+  FormData["username"]=document.getElementById("username").value; 
+  FormData["phone"]=document.getElementById("phone").value; 
+  FormData["email"]=document.getElementById("email").value; 
 
-var formData;
-function getdata()
-{
-    var formData={
-        id:x,
-        name:document.getElementById("name").value,
-        username:document.getElementById("username").value,
-        phone:document.getElementById("phone").value,
-        email:document.getElementById("email").value
-      }
-      console.log(formData);
-        var formdata={};
-        formdata["id"]=x;
-        formdata["name"]=document.getElementById("name").value;
-        formdata["username"]=document.getElementById("username").value;
-        formdata["email"]=document.getElementById("email").value;
-        formdata["phone"]=document.getElementById("phone").value;
-        return formdata;
+  return FormData;
 }
 function insertdata(data)
 {
@@ -91,56 +102,64 @@ function onDelete(td) {
             resetdata();
         }
 }
-var newData2;
 function fetchData(){
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then(res => {res.json()
-        .then(data =>{console.log(data);
-        if(data.length>0)
+  if(localStorage.getItem("data")===null)
+  {
+    fetch("https://jsonplaceholder.typicode.com/users").then(
+      res =>{
+        res.json().then(
+          data=>{
+            if(data.length>0)
             {
               var temp="";
-              var local_data="";
               if(!localStorage.getItem("data")){
                 localStorage.setItem('data',data)
               }
               else
               {
               let newData= localStorage.getItem('data')
-              newData2=JSON.parse(newData);
+              let local_data=JSON.parse(newData);
               for(let i=0;i<data.length;i++)
               {
                 temp+="<tr>";
-                temp+="<td>"+newData2[i].id+"</td>";
-                temp+="<td>"+newData2[i].name+"</td>";
-                temp+="<td>"+newData2[i].username+"</td>";
-                temp+="<td>"+newData2[i].phone+"</td>";
-                temp+="<td>"+newData2[i].email+"</td>";
+                temp+="<td>"+local_data[i].id+"</td>";
+                temp+="<td>"+local_data[i].name+"</td>";
+                temp+="<td>"+local_data[i].username+"</td>";
+                temp+="<td>"+local_data[i].phone+"</td>";
+                temp+="<td>"+local_data[i].email+"</td>";
                 temp+='<td><button class="btn btn-primary" onClick="onEdit(this)" data-toggle="modal" data-target="#exampleModal">Edit</button> <button class="btn btn-primary" onClick="onDelete(this)">Delete</button></td></tr>';
               }
-
-              //console.log(newData);
-              }
+            }
               x= data.length + 1;
-                // var temp="";
-                // data.forEach((u)=>{
-                //     temp += "<tr>";
-                //     temp += "<td>"+u.id+"</td>"
-                //     temp += "<td>"+u.name+"</td>"
-                //     temp += "<td>"+u.username+"</td>"
-                //     temp += "<td>"+u.email+"</td>"
-                //     temp += "<td>"+u.phone+"</td>"
-                //     temp += '<td><button  onClick="onEdit(this)" data-toggle="modal" data-target="#exampleModal">Edit</button> <button  onClick="onDelete(this)">Delete</button></td></tr>' 
-                // })
-                document.getElementById("data").innerHTML = temp;
-                let myobj=JSON.stringify(data);
-                localStorage.setItem("data",myobj)
-                // let myobj1=JSON.parse(myobj);
-                // console.log(myobj1[1].id)
-
-        }
+              document.getElementById("data").innerHTML=temp;
+              let myObj = JSON.stringify(data);
+              localStorage.setItem("data", myObj);
+              
+            }
+          }
+        )
       }
-        )
+    )
+  }
+else{
+  let newData= localStorage.getItem('data')
+  var temp=""
+  let local_data=JSON.parse(newData);
+  for(let i=0;i<local_data.length;i++)
+    {
+        temp+="<tr>";
+        temp+="<td>"+local_data[i].id+"</td>";
+        temp+="<td>"+local_data[i].name+"</td>";
+        temp+="<td>"+local_data[i].username+"</td>";
+        temp+="<td>"+local_data[i].phone+"</td>";
+        temp+="<td>"+local_data[i].email+"</td>";
+        temp+='<td><button class="btn btn-primary" onClick="onEdit(this)" data-toggle="modal" data-target="#exampleModal">Edit</button> <button class="btn btn-primary" onClick="onDelete(this)">Delete</button></td></tr>';
+   }
+    x= local_data.length + 1;
+    console.log(local_data)
+    document.getElementById("data").innerHTML=temp;
+    let myObj = JSON.stringify(data);
+    localStorage.setItem("data", myObj);
+    }
 }
-        )
-}
-      
+fetchData()
